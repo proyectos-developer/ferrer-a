a6daa8b8dce3297a2c80d50d84c923328b4a91de
proyectos-developer <proyectos@developer-ideas.com>
2024-07-07 16:56:47 -0500

    V1.0.0

--- app.js
+++ app.js
@@ -73,6 +73,41 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './client/build', 'index'));
 });
 
+app.use(express.static(path.resolve(__dirname, './client/build/sobre-nosotros')));
+app.get('/sobre-nosotros', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build/sobre-nosotros', 'index'));
+});
+
+app.use(express.static(path.resolve(__dirname, './client/build/nuestra-tienda')));
+app.get('/nuestra-tienda', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build/nuestra-tienda', 'index'));
+});
+
+app.use(express.static(path.resolve(__dirname, './client/build/producto')));
+app.get('/producto/:producto/:sku', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build/producto', 'index'));
+});
+
+app.use(express.static(path.resolve(__dirname, './client/build/contactanos')));
+app.get('/contactanos', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build/contactanos', 'index'));
+});
+
+app.use(express.static(path.resolve(__dirname, './client/build/carrito-compras')));
+app.get('/carrito-compras', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build/carrito-compras', 'index'));
+});
+
+app.use(express.static(path.resolve(__dirname, './client/build/sesion')));
+app.get('/sesion', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build/sesion', 'index'));
+});
+
+app.use(express.static(path.resolve(__dirname, './client/build/olvide-password')));
+app.get('/olvide-password', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build/olvide-password', 'index'));
+});
+
 //Iniciar el servidor
 app.listen (app.get('port'), () => {
     console.log ('Server en puerto ', app.get ('port'))
